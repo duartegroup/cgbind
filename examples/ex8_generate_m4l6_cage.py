d8402b9eb6f7dca0a7b0d30b19fb142e3209f5ff
Unknown <ty13946@my.bristol.ac.uk>
2019-08-21 12:06:00 +0100

    Auto stash before merge of "master" and "origin/master"

--- examples/ex8_generate_m4l6_cage.py
+++ examples/ex8_generate_m4l6_cage.py
@@ -7,4 +7,4 @@ gen_cage(linker_name='Lx',
          linker_smiles='O=C(C1=C([O-])C([O-])=CC=C1)NC2=CC=CC3=C2C=CC=C3NC(C4=C([O-])C([O-])=CC=C4)=O',
          metal_label='Ga',
          metal_charge=3,
-         arch='m4l6')
+         arch=M4L6)
