921b067a1d46621882690d5a593076f744dc1261
Unknown <ty13946@my.bristol.ac.uk>
2020-01-03 09:28:25 +0000

    Bug fix

--- cgbind/__init__.py
+++ cgbind/__init__.py
@@ -3,7 +3,6 @@ from cgbind import geom
 from cgbind.constants import Constants
 from cgbind.linker import Linker
 from cgbind.cage import Cage
-from cgbind.config import Config
 from cgbind.substrate import Substrate
 from cgbind.cage_subt import CageSubstrateComplex
 from cgbind.templates import Template
@@ -17,7 +16,6 @@ __version__ = '1.0.0'
 __all__ = [
     'Constants',
     'defaults',
-    'Config',
     'geom',
     'Linker',
     'Cage',
