fcee0901736da990d8817e6c03cbb249228afa9b
Unknown <ty13946@my.bristol.ac.uk>
2020-01-03 09:11:40 +0000

    Migrated to travis doc building

--- doc/conf.py
+++ doc/conf.py
@@ -11,6 +11,8 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 # -- Project information -----------------------------------------------------
+import sphinx_rtd_theme
+
 autodoc_default_options = {
     'members':         True,
     'special-members': '__init__'
@@ -30,7 +32,8 @@ release = '1.0.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc']
+extensions = ['sphinx.ext.autodoc',
+              'sphinx_rtd_theme']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -46,7 +49,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
