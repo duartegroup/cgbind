4bce68226cb495c67ae92b1ed2852e25ad2c116b
Tom <tom.young@balliol.ox.ac.uk>
2020-06-16 11:30:35 +0100

    Updates for pip distribution and allow no permutation in build

--- cgbind/config.py
+++ cgbind/config.py
@@ -4,3 +4,6 @@ class Config:
     """
 
     n_cores = 1    #: Maximum number of cores to use throughout the code
+
+    #: Allow permutations of Xmotifs to minimise linker-linker repulsion
+    allow_permutations = True
--- setup.py
+++ setup.py
@@ -1,26 +1,35 @@
-from setuptools import setup, Command
+from setuptools import setup
 from Cython.Build import cythonize
 from setuptools.extension import Extension
 import sys
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+extensions = []
 
 # By default don't build the ESP C++ extension on Windows
-if '--esp_gen' not in sys.argv and sys.platform == 'win32':
-    extensions = []
-else:
-    extensions = [Extension('esp_gen', ['cgbind/ext/esp_gen.pyx'])]
+if '--esp_gen' in sys.argv or sys.platform != 'win32':
+
+    # Try to build the extension from the Cython generated C
+    if os.path.exists(os.path.join(here, 'cgbind', 'ext', 'esp_gen.c')):
+        ext = 'c'
+    else:
+        ext = 'pyx'
+
+    extensions.append(Extension('esp_gen', [f'cgbind/ext/esp_gen.{ext}']))
 
 if '--esp_gen' in sys.argv:
     sys.argv.remove('--esp_gen')
 
 
 setup(name='cgbind',
-      version='1.0.0',
+      version='1.0.0a',
+      description='Metallocage construction and binding affinity calculations',
       packages=['cgbind'],
-      include_package_data=True,
       package_data={'': ['lib/*']},
       ext_modules=cythonize(extensions, language_level="3", annotate=True),
-      url='',
+      url='https://github.com/duartegroup/cgbind',
       license='MIT',
       author='Tom Young',
       author_email='tom.young@chem.ox.ac.uk')
-
